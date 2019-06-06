8ee1fa9d08d93b69cf5442a4eb289fbcc10490f3
Bill Kunneke <bill.kunneke@gmail.com>
2019-06-06 15:24:34 -0400

    Intial Commit

---
+++ mongodb.js
@@ -0,0 +1,29 @@
+// CRUD operations
+
+const { MongoClient, ObjectID } = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL,{ useNewUrlParser: true}, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database')
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('tasks').deleteOne({
+        description: 'Take node.js course'
+    }).then((result) => {
+        console.log(result.deletedCount)
+    }).catch((error) => {
+        console.log(error)
+    })
+    // db.collection('users').deleteMany({
+    //     age: 27
+    // }).then((result) =>{
+    //     console.log(result.deletedCount)
+    // }).catch((error) =>{
+    //     console.log(error)
+    // })
+})
